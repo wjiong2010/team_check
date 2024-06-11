a596afa0bed61bd31380c9ab3c4a13e2a98278ad
John Wang <wjiong2010@163.com>
2024-06-11 13:32:02 +0800

    [kpi]添加KPI处理的开始和结束提示。

--- source/team_check.py
+++ source/team_check.py
@@ -20,9 +20,11 @@ def main():
     # software_develop_team.save_as_text("cr_result", "Code_Review.txt")
     software_develop_team.save_as_excel("cr_result", "Code_Review.xlsx")
 
-    kpi_csv_file_list = ["redmin_0101-0331.csv", "PMS_0101-0331.csv"]
+    print("KPI processing start")
+    kpi_csv_file_list = ["redmin_0401-0605.csv", "PMS_0401-0605.csv"]
     kpi_process(root_path, kpi_csv_file_list, software_develop_team.members)
-    software_develop_team.save_as_text("kpi", "2024Q1-KPI_Report.txt")
+    software_develop_team.save_as_text("kpi", "2024Q2-KPI_Report.txt")
+    print("KPI processing end")
 
 
 # 外部调用的时候不执行
