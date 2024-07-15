d6e9dee50cce7a825e03291cfbdc9f5e806d57e9
John Wang <wjiong2010@163.com>
2024-07-15 10:06:03 +0800

    代码review结果按照周建立目录， 解析结果也放在xml同目录下。

--- source/team_check.py
+++ source/team_check.py
@@ -9,23 +9,26 @@ software_develop_team = Team()
 
 def main():
     season = 'Q2'
+    cr_week = '0715'
     path = os.getcwd()
     root_path = path.replace("source", "raw_data")
-    root_path = os.path.join(root_path, season)
     print(f"root_path: {root_path}")
 
     software_develop_team.init_members()
     # software_develop_team.print_members()
 
-    # cr_xml_file_list = ["Code_Review.xml", "Code_Review_pro.xml"]
-    # cr_parse_result(root_path, cr_xml_file_list, software_develop_team)
-    # # software_develop_team.save_as_text("cr_result", "Code_Review.txt")
-    # software_develop_team.save_as_excel("cr_result", "Code_Review.xlsx")
+    cr_path = os.path.join(root_path, "cr_" + cr_week)
+    cr_xml_file_list = ["Code_Review.xml", "Code_Review_pro.xml"]
+    cr_parse_result(cr_path, cr_xml_file_list, software_develop_team)
+    # software_develop_team.save_as_text("cr_result", "Code_Review.txt")
+    cr_xlsx = os.path.join(cr_path, "Code_Review_" + cr_week + ".xlsx")
+    software_develop_team.save_as_excel("cr_result", cr_xlsx)
 
     print("KPI processing start")
+    kpi_path = os.path.join(root_path, season)
     kpi_csv_file_list = ["PMS_0101-0630.csv"]
-    kpi_process(root_path, kpi_csv_file_list, software_develop_team.members)
-    output_file = os.path.join(root_path, "2024Q2-REQ-BUG_Report.txt")
+    kpi_process(kpi_path, kpi_csv_file_list, software_develop_team.members)
+    output_file = os.path.join(kpi_path, "2024Q2-REQ-BUG_Report.txt")
     software_develop_team.save_as_text("kpi", output_file)
     print("KPI processing end")
 
